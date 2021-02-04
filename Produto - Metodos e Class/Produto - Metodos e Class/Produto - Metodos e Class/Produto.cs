ef8eb8f7699a4bc118b38bb37e1214c599b077ac
Biel Steve <gabrielsteve@uniplaclages.edu.br>
2021-02-03 23:29:07 -0300

    Criando programinha utilizando Classes, Atributos e Metodos

---
+++ Produto - Metodos e Class/Produto - Metodos e Class/Produto - Metodos e Class/Produto.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Produto___Metodos_e_Class
+{
+    class Produto
+    {
+        public string Nome;
+        public double Preco;
+        public int Quantidade;
+
+        //metodo
+        public double ValorTotalEstoque()
+        {
+            return Preco * Quantidade;
+        }
+    }
+}
---
+++ Produto - Metodos e Class/Produto - Metodos e Class/Produto - Metodos e Class/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Globalization;
+
+namespace Produto___Metodos_e_Class
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Produto - Metodos e Class
+            //Biel Steve
+
+            Produto p = new Produto();
+
+            Console.WriteLine("Entre com os dados do Produto: ");
+            Console.Write("Nome: ");
+            p.Nome = Console.ReadLine();
+            Console.Write("Preço: ");
+            p.Preco = double.Parse(Console.ReadLine());
+            Console.Write("Quantidade no estoque: ");
+            p.Quantidade = int.Parse(Console.ReadLine());
+        }
+    }
+}
