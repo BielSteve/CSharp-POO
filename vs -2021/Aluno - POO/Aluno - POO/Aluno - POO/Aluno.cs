0ceb857dd9d30246a5cecfe2af39fd574d0552d3
Gabriel do Nascimento Pires <gabriel.pires@ndd.com.br>
2021-02-05 11:05:51 -0300

    Exercicios com classes, metodos, atributos e bool

---
+++ vs -2021/Aluno - POO/Aluno - POO/Aluno - POO/Aluno.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Globalization;
+
+namespace Aluno___POO
+{
+    class Aluno
+    {
+
+        public string Nome;
+        public double N1, N2, N3;
+
+        public double NotaFinal()
+        {
+            double notafinal = N1 + N2 + N3;
+            return notafinal;
+        }
+
+        public bool Aprovado()
+        {
+            if (NotaFinal() >= 60.0)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        public double NotaRestante()
+        {
+            if (Aprovado())
+            {
+                return 0.0;
+            }
+            else
+            {
+                return 60.0 - NotaFinal();
+            }
+        }
+
+
+
+
+
+
+    }
+}
---
+++ vs -2021/Aluno - POO/Aluno - POO/Aluno - POO/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Globalization;
+
+namespace Aluno___POO
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Aluno - POO
+            //Biel SSteve
+
+            Aluno n = new Aluno();
+
+            Console.Write("Nome do Aluno : ");
+            n.Nome = Console.ReadLine();
+            Console.WriteLine("Digite as 3 notas do aluno: ");
+            string[] s = Console.ReadLine().Split(' ');
+            n.N1 = double.Parse(s[0], CultureInfo.InvariantCulture);
+            n.N2 = double.Parse(s[1], CultureInfo.InvariantCulture);
+            n.N3 = double.Parse(s[2], CultureInfo.InvariantCulture);
+
+            Console.WriteLine("NOTA FINAL: "+n.NotaFinal().ToString("F2", CultureInfo.InvariantCulture));
+
+            if (n.Aprovado())
+            {
+                Console.WriteLine("APROVADO");
+            }
+            else
+            {
+                Console.WriteLine("REPROVADO");
+                Console.WriteLine("FALTARAM "
+                + n.NotaRestante().ToString("F2", CultureInfo.InvariantCulture)
+                + " PONTOS");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
