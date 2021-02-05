0336270244de51b62a67816bb1da9e3831d41e86
Biel Steve <gabrielsteve@uniplaclages.edu.br>
2021-02-04 23:38:25 -0300

    Exercicios com Classes, Atributos e Metodos/ Object ToString

---
+++ vs -2021/Funcionario/Funcionario/Funcionario/Funcionario.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Globalization;
+
+namespace Funcionario
+{
+    class Funcionario
+    {
+        public string Nome;
+        public double SalarioBruto;
+        public double Imposto;
+
+
+        public double SalarioLiquido()
+        {
+            double salario_liquido = SalarioBruto - Imposto;
+            return salario_liquido;
+        }
+
+        public void AumentarSalario(double porcentagem)
+        {
+            SalarioBruto = SalarioBruto + (SalarioBruto * (porcentagem / 100));
+        }
+        public override string ToString()
+        {
+            return Nome + " , R$" + SalarioLiquido().ToString("F2", CultureInfo.InvariantCulture);
+        }
+    }
+        
+    
+}
---
+++ vs -2021/Funcionario/Funcionario/Funcionario/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Globalization;
+
+namespace Funcionario
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Funcionario
+            //Biel Steve
+
+            Funcionario f = new Funcionario();
+
+            Console.Write("Nome: ");
+            f.Nome = Console.ReadLine();
+            Console.Write("Salario Bruto: ");
+            f.SalarioBruto = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.Write("Imposto: ");
+            f.Imposto = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.WriteLine();
+            
+            Console.Write("Funcionario: "+f);
+            Console.WriteLine();
+
+            Console.WriteLine("Digite a porcentagem para aumentar o salário:");
+            double por = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            f.AumentarSalario(por);
+
+            Console.WriteLine();
+            Console.Write("Dados atualizados: " + f);
+
+
+
+
+
+
+        }
+    }
+}
