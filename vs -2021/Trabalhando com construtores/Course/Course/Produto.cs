ecf4ddda65adfaf53cc1665287b41fa60a5586ca
Biel Steve <gabrielsteve@uniplaclages.edu.br>
2021-02-09 23:24:41 -0300

    Introdução a construtores

---
+++ vs -2021/Trabalhando com construtores/Course/Course/Produto.cs
@@ -0,0 +1,52 @@
+﻿using System.Globalization;
+namespace Course
+{
+    class Produto
+    {
+        public string Nome;
+        public double Preco;
+        public int Quantidade;
+
+        //Isso é um construtor, ele sempre tera o mesmo nome da classe
+        //Nesse caso estamos obrigando ao usuario ter que digitar sempre esses valores de entrada
+        public Produto(string nome, double preco, int quantidade)
+        {
+            Nome = nome;
+            Preco = preco;
+            Quantidade = quantidade;
+        }
+        //Criando mais um construtor com parametros diferentes, nesse caso gera sobrecarga
+        public Produto(string nome, double preco)
+        {
+            Nome = nome;
+            Preco = preco;
+            Quantidade = 5;
+        }
+        //Também é possivel criar um construtor vazio
+        public Produto()
+        {
+        }
+        public double ValorTotalEmEstoque()
+        {
+            return Preco * Quantidade;
+        }
+        public void AdicionarProdutos(int quantidade)
+        {
+            Quantidade += quantidade;
+        }
+        public void RemoverProdutos(int quantidade)
+        {
+            Quantidade -= quantidade;
+        }
+        public override string ToString()
+        {
+            return Nome
+            + ", $ "
+            + Preco.ToString("F2", CultureInfo.InvariantCulture)
+            + ", "
+            + Quantidade
+            + " unidades, Total: $ "
+            + ValorTotalEmEstoque().ToString("F2", CultureInfo.InvariantCulture);
+        }
+    }
+}
