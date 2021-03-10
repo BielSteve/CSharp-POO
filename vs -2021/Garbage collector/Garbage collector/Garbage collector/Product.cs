c8db571546400a8e518a65677c8786778022a10f
Biel Steve <gabrielsteve@uniplaclages.edu.br>
2021-03-09 23:52:37 -0300

    Apredendo gargabe collector

---
+++ vs -2021/Garbage collector/Garbage collector/Garbage collector/Product.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Garbage_collector
+{
+    class Product
+    {
+        public string name;
+        public double preco;
+        public int qtda;
+
+        public Product(string name, double preco, int qtda)
+        {
+            this.name = name;
+            this.preco = preco;
+            this.qtda = qtda;
+        }
+
+        public string method1()
+        {
+            Product p = method2();
+            string prod = p.name;
+            return prod;
+            
+        }
+
+        public Product method2()
+        {
+            Product prod = new Product("TV", 900.00, 0);
+            return prod;
+        }
+        public override string ToString()
+        {
+            return name+" " + preco + " " + qtda;
+        }
+    }
+}
---
+++ vs -2021/Garbage collector/Garbage collector/Garbage collector/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Garbage_collector
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Garbage collector
+            //Biel Steve
+            Product p = new Product("TV", 900.00, 0);
+
+            Console.WriteLine(p.method1());
+        }
+
+       
+
+
+    }
+}
