39c858966c5b85e3374f0cc877b84b39c85abeda
Biel Steve <gabrielsteve@uniplaclages.edu.br>
2021-03-15 23:23:42 -0300

    Vetores do tipo Classe

---
+++ vs -2021/Vetor do tipo Classe/Vetor do tipo Classe/Vetor do tipo Classe/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Globalization;
+
+namespace Vetor_do_tipo_Classe
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Vetor do tipo Classe 
+            //Biel Steve
+
+            Console.WriteLine("Digite a quantidade :");
+            int n = int.Parse(Console.ReadLine());
+
+            Product[] vet = new Product[n];
+
+            Console.WriteLine("Digite o nome e o preço : ");
+            for (int i=0; i<n; i++)
+            {
+                string name = Console.ReadLine();
+                double price = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                vet[i] = new Product { Name = name, Price = price };
+            }
+            double soma = 0.0;
+
+            for (int i = 0; i<n; i++)   
+            {
+                soma = soma + vet[i].Price;
+            }
+            double media = soma / n;
+
+            Console.WriteLine("Preço médio igual a: " + media.ToString("F2", CultureInfo.InvariantCulture));
+            Console.ReadLine();
+        }
+    }
+}
---
+++ vs -2021/Vetor/Vetor/Vetor/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Globalization;
+
+namespace Vetor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Vetor
+            //Biel Steve
+
+            int n;
+
+
+
+            Console.WriteLine("Digite uma quantidade :");
+            n = int.Parse(Console.ReadLine());
+            double[] vetor = new double[n];
+
+
+            for (int i = 0; i < n; i++)
+            {
+                vetor[i] = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            }
+            double media = 0.0;
+
+            for (int i = 0; i < n; i++)
+            {
+                media = media + vetor[i];
+            }
+
+            double resul = media / n;
+            Console.WriteLine("A media é : " + resul.ToString("F2", CultureInfo.InvariantCulture));
+        }
+    }
+}
