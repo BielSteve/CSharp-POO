db9ca661dd0b41e04fb7f393fcc90a57237f7f3f
Biel Steve <gabrielsteve@uniplaclages.edu.br>
2021-03-20 17:28:49 -0300

    Exercicio com vetores do tipo classe

---
+++ vs -2021/Exercicio(vetores)/Exercicio(vetores)/Exercicio(vetores)/Aluguel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Exercicio_vetores_
+{
+    class Aluguel
+    {
+
+        public string Nome { get; set; }
+        public string Email { get; set; }
+        public int Quarto { get; set; }
+
+        //Construtor
+        public Aluguel(string nome, string email)
+        {
+            Nome = nome;
+            Email = email;
+        }
+
+        public override string ToString()
+        {
+            return Nome + ", " + Email;
+        }
+    }
+}
---
+++ vs -2021/Exercicio(vetores)/Exercicio(vetores)/Exercicio(vetores)/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace Exercicio_vetores_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Exercicio(vetores)
+            //Biel Steve
+
+            Console.WriteLine("Quantos quartos vão ser alugados? ");
+            int n = int.Parse(Console.ReadLine());
+
+            Aluguel[] vet = new Aluguel[10];
+
+            for (int i = 1; i<=n; i++)
+            {
+                Console.WriteLine("Aluguel #" + i);
+                Console.Write("Nome: ");
+                string nome = Console.ReadLine();
+                Console.Write("Email: ");
+                string email = Console.ReadLine();
+                Console.Write("Quarto: ");
+                int quarto = int.Parse(Console.ReadLine());
+                vet[quarto] = new Aluguel (nome, email);
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("Quartos ocupados:");
+            for (int i= 0; i<10; i++)
+            {
+                if (vet[i] != null)
+                {
+                    Console.WriteLine(i + " : " + vet[i]);
+                }
+            }
+
+        }
+    }
+}
